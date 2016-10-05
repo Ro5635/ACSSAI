8ba0954cd0b11d26778f4362a5da5155756bd7b3
Ro5635 <ro5635@gmail.com>
2016-10-05 18:48:02 +0100

    BareBones Version
    Maintain an old limited working prototype of the interface without any
    3D content.

---
+++ Site/MVC/views/pages/mini.php
@@ -0,0 +1,7 @@
+ 
+
+<img onclick="startDictation()" id="MinionImg" src="https://s-media-cache-ak0.pinimg.com/564x/48/bd/3f/48bd3f6e928d7cb4b8d499cb0f96b8a8.jpg" />
+
+<br><br>
+<div id="transcript">
+</div>
